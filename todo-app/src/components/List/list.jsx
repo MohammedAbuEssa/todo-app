aed8ad0a7a81bc3400667c9cff0914fc5909843e
MohammedAbuEssa <mabuissa5@gmail.com>
2023-09-05 06:50:19 +0000

    Cnotext methods

--- todo-app/src/components/List/list.jsx
+++ todo-app/src/components/List/list.jsx
@@ -1,31 +1,38 @@
-import React,{useState, useContext, useEffect } from 'react'
+import React,{useEffect, useState} from 'react'
 import {Pagination} from '@mui/material';
 
 
 export default function list({list,setting}) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [displayedItems, setDisplayedItems] = useState([]);
   const handlePageChange = (event, newPage) => {
     setCurrentPage(newPage);
   };
-
-    const startIndex = (currentPage - 1) * setting.display;
-    const endIndex = startIndex + setting.display;
-    let displayedItems= list.slice(startIndex, endIndex);
-    console.log(displayedItems);
-
-
+  useEffect(()=>{
+    const num = setting.display
+    list.sort((a,b) => {return (a[setting.sort] - b[setting.sort])})
+    const startIndex = (currentPage - 1) * num;
+    const endIndex = parseInt(startIndex) + parseInt(num);
+    setDisplayedItems(list.slice(startIndex, endIndex));
+    // console.log(list)
+    console.log('num = ',num);
+    console.log('startIndex = ',startIndex);
+    console.log('endIndex = ',endIndex);
+  },[list,currentPage,setting])
+ 
+  
 
   return (
     <div className='list'>
-      {displayedItems.map(item => (
-        (setting.hideCompleted? !item.complete : true) && <div className='listItem' key={item.id}>
+      {displayedItems.map((item,idx) => (
+        (setting.hideCompleted? !item.complete : true) && <div className='listItem' key={idx}>
           <p>{item.text}</p>
           <p><small>Assigned to: {item.assignee}</small></p>
           <p><small>Difficulty: {item.difficulty}</small></p>
           <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
           <hr />
         </div>
-
+        
         ))}
         <div className='pagination'>
         <Pagination 
--- todo-app/src/context/Settings/index.jsx
+++ todo-app/src/context/Settings/index.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 export const SettingContext = React.createContext();
 export default function Settings(props){
+    const [display,setDisplay]= useState(3)
+    const [hideCompleted,setHideCompleted]= useState(false)
+    const [sort,setSort]= useState('difficulty')
     const state = {
-        display:3,
-        hideCompleted : false,
-        difficulty:3,
+        display:display,
+        hideCompleted : hideCompleted,
+        sort:sort,
+        setDisplay:setDisplay,
+        setHideCompleted:setHideCompleted,
+        setSort:setSort,
     }
+   
+
     return (
         <SettingContext.Provider value={state}>
             {props.children}
